a145952695f1e97190b392850fcf41fa4d3e4548
Savas Pamuk <savaspamuk@gmail.com>
2024-01-03 21:34:33 +0100

    feat: create portfolio component

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid } from "@mui/material";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Resume from "./pages/Resume/Resume";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={4} lg={3}>
+            <Profile />
+          </Grid>
+          <Grid item xs style={{ backgroundColor: "red" }}>
+            <Header />
+            <Routes>
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/" element={<Resume />} />
+            </Routes>
+            <Footer />
+          </Grid>
+        </Grid>
+      </Container>
+    </Router>
   );
 }
 
